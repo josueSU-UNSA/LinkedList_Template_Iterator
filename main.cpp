04a1ef4b4ebfa82a981c17fc5987e67e917ae14d
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2021-07-20 18:51:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<list>
+#include<conio.h>
 #include "Iterator.h"
 using namespace std;
 int main(){
@@ -16,7 +18,7 @@ int main(){
 
     cout<<lista<<endl<<endl;
     cout<<"Usando un for para iterar sobre la lista mediante indices"<<endl;
-    for (int i=0;i<lista.getLongitud();i++){
+    for (int i=0;i<lista.getLongitud();++i){
         cout<<*lista[i]<<endl;
     }
     cout<<endl<<endl;
@@ -38,11 +40,26 @@ int main(){
     cout<<lista<<endl;
     cout<<"Su nueva longitud seria "<<lista.getLongitud()<<endl;
     
-    cout<<"Probando iterador "<<endl;
+    cout<<"Probando objeto iterador en la lista "<<endl;
     Iterator<int> iterador;
     for(iterador=lista.begin();iterador!=nullptr;iterador++){
         cout<<iterador;
     }
-    cout<<"Aca termina de probarse el code"<<endl;
+    cout<<"Borramos el primer y ultimo elemento de la lista"<<endl;
+    lista.eliminarBegin();
+    lista.eliminarEnd();
+    cout<<lista<<endl;
+    cout<<"La longitud de la lista actualizada es "<<lista.getLongitud()<<endl<<endl;
+    list<int>pruebaStl();
+    
+    cout<<"Antes de invocar al destructor: "<<endl;
+    (lista.vacio())?cout<<"La lista esta vacia "<<endl:cout<<"La lista no esta vacia "<<endl<<endl;
+    
+    lista.~LinkedList();
+    cout<<"Despues de invocar al destructor: "<<endl;
+    (lista.vacio())?cout<<"La lista esta vacia "<<endl:cout<<"La lista no esta vacia "<<endl;
+    
+    
+    getch();
     return 0;
 }
